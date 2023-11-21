921d7be1f487fedfab2261d29bf319f5208c33d1
Paul Tuck <paul.tuck@oxon.org>
2023-11-21 20:24:59 +0700

    "2023-11-21_20:24_UTC+7"

--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -4,12 +4,18 @@ import { HydratedDocument } from "mongoose";
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
-export class User {
+export class User extends Document {
 	@Prop({ required: true })
 	id: string;
 
-	@Prop()
+	@Prop({ unique: true })
+	username: string;
+
+	@Prop({ unique: true, required: true })
 	email: string;
+
+	@Prop()
+	password: string; // Hashed password
 
 	@Prop()
 	givenName: string;
@@ -20,8 +26,77 @@ export class User {
 	@Prop()
 	preferredName: string;
 
+	@Prop()
+	profilePicture: string; // URL or path to the image
+
+	// @Prop()
+	// bio: string;
+
+	@Prop()
+	birthDate: Date;
+
+	@Prop()
+	gender: string;
+
+	@Prop()
+	phoneNumber: string;
+
+	// Address fields (can also be a sub-document)
+	// @Prop()
+	// addressLine1: string;
+
+	// @Prop()
+	// addressLine2: string;
+
+	@Prop()
+	city: string;
+
+	@Prop()
+	state: string;
+
+	// @Prop()
+	// postalCode: string;
+
+	@Prop()
+	country: string;
+
+	// Security and compliance
+	@Prop({ default: false })
+	verified: boolean;
+
+	@Prop({ default: false })
+	twoFactorEnabled: boolean;
+
+	// User preferences
+	@Prop()
+	language: string;
+
+	@Prop()
+	locale: string;
+
+	@Prop()
+	timeZone: string;
+
+	// Roles and permissions
 	@Prop({ type: [String], default: [] })
 	roles: string[];
+
+	// Account status
+	@Prop({ default: true })
+	isActive: boolean;
+
+	@Prop()
+	isSuspended: boolean;
+
+	// Timestamps
+	@Prop({ default: Date.now })
+	createdAt: Date;
+
+	@Prop({ default: Date.now })
+	updatedAt: Date;
+
+	@Prop()
+	lastLogin: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
